fb97c9a26f9b0464fd017da19df11258502ece0d
Regina Lionheart <regina@lionhe.art>
2019-06-03 15:14:56 -0700

    Use sapply to change multiple variable types in raw areas dataframe.

--- skylineQC_QE.R
+++ skylineQC_QE.R
@@ -36,6 +36,7 @@
 # TODO (lionhearts): What specifically is happening in the ID run type section, as well as the RT.matrix section? 
 # TODO (lionhearts): Where is the range coming from in RT.range under Range of Retention Times?
 # TODO (lionhearts): The if statement on line 137 seems strange. Why pull "poo" from run.type.options?
+# TODO (lionhearts): Why does print(paste Variables) line in testing.R return two instances of the $Replicate.Name column?
 
 library(dplyr)
 library(GGally)
@@ -45,11 +46,10 @@ library(stringr)
 library(tidyr)
 library(tidyverse)
 
+
 main <- function() {
   args <- commandArgs(trailingOnly = TRUE)
   filename <- args[1]
-  # actions <- args[-1]
-  # stopifnot(action %in% c("--filter", "--extract"))
   
   if (length(filename) == 0) {
     filtering(file("stdin"))
@@ -97,7 +97,7 @@ filtering <- function(filename) {
     filter(Precursor.Ion.Name != "GTP",
            Precursor.Ion.Name != "Argininosuccinic Acid",
            Precursor.Ion.Name != "Cys-Gly")
-  areas.raw.noIS <- areas.raw %>%
+  areas.raw.noIS <<- areas.raw %>%
     filter(!Protein.Name == "Internal Stds_neg")
   
   print(head(areas.raw))
@@ -111,188 +111,193 @@ main()
 ## Identify run types ---------------------------
 # Standards (std), Samples (smp), Blanks (blk), Pooled (poo)
 ID_runtypes <- function() {
-  run.type <- tolower(str_extract(areas.raw.noIS$Replicate.Name, "(?<=_)[^_]+(?=_)"))
+  run.type <- as.factor(tolower(str_extract(areas.raw.noIS$Replicate.Name, "(?<=_)[^_]+(?=_)")))
+  areas.raw.noIS.test[ , "Sample.Type"] <- run.type
   print(head(run.type))
 }
 
 ## Change variable types ---------------------------------------------------
-areas.raw.noIS$sample.type        <- as.factor(run.type)
+before <- sapply(areas.raw.noIS[-1], class)
 areas.raw.noIS$Precursor.Ion.Name <- as.factor(areas.raw.noIS$Precursor.Ion.Name)
-areas.raw.noIS$Area               <- as.numeric(areas.raw.noIS$Area)
-areas.raw.noIS$Retention.Time     <- as.numeric(areas.raw.noIS$Retention.Time)
-areas.raw.noIS$Background         <- as.numeric(areas.raw.noIS$Background)
-areas.raw.noIS$Height             <- as.numeric(areas.raw.noIS$Height)
-areas.raw.noIS$Mass.Error.PPM     <- as.numeric(areas.raw.noIS$Mass.Error.PPM)
+cols.to.change <- c(7:9, 12)
+areas.raw.noIS[cols.to.change] <- sapply(areas.raw.noIS[cols.to.change], as.numeric)
+sapply(areas.raw.noIS[-1], class)
+print(before, after)
 
-stdRows <- areas.raw.noIS$sample.type == "std"
-blkRows <- areas.raw.noIS$sample.type == "blk"
 
-areas.split <- split(areas.raw.noIS, areas.raw.noIS$sample.type)
-
-run.type.options <- names(areas.split)
-
-cmpd.blk.list <- split(areas.split[["blk"]],
-                       areas.split[["blk"]]$Precursor.Ion.Name)
- 
+# Might not need these?
+# stdRows <- areas.raw.noIS$sample.type == "std"
+# blkRows <- areas.raw.noIS$sample.type == "blk"
+#
+# areas.split <- split(areas.raw.noIS, areas.raw.noIS$sample.type)
+#
+# run.type.options <- names(areas.split)
+#
+# # # Might not need this either?
+# # cmpd.blk.list <- split(areas.split[["blk"]],
+# #                        areas.split[["blk"]]$Precursor.Ion.Name)
+#
 # ## Check the range of Retention Times and ion ratio in Standards ---------------------
 # # Range of Retention Times (RTs) and pooled sample inclusion
-# RT.range <- sapply(split(areas.split[["std"]]$Retention.Time, 
+#
+#
+# RT.range <- sapply(split(areas.split[["std"]]$Retention.Time,
 #                          areas.split[["std"]]$Precursor.Ion.Name),
 #                          range, na.rm = T)
-# 
-# blk.range <- sapply(split(areas.split[["blk"]]$Area, 
+#
+# blk.range <- sapply(split(areas.split[["blk"]]$Area,
 #                           areas.split[["blk"]]$Precursor.Ion.Name),
-#                           range, na.rm = T) 
-# 
+#                           range, na.rm = T)
+#
 # samp.data <- areas.split[["smp"]]
 # blank.data <- areas.split[["blk"]]
-# 
+#
 # # If there are pooled samples, include them in the 'sample' list so only blanks and standards are excluded.
 # if (any(run.type.options == "poo")) {
 #      poo.data <- areas.split[["poo"]]
 #      samp.data <- rbind(samp.data, poo.data)
 # }
-# 
+#
 # cmpds <- unique(samp.data$Precursor.Ion.Name)
 # samples <- unique(samp.data$Replicate.Name)
 # cmpd.samp.dfs <- split(samp.data, samp.data$Precursor.Ion.Name)
-# 
-# ## RT range matrix ------------------------------
-# RT.ok <- RT.range
-# RT.ok[1, ] <- RT.range[1, ] - RT.flex
-# RT.ok[2, ] <- RT.range[2, ] + RT.flex
-# 
-# # Get the sample RTs
-# RT.matrix <- c()
-# for (i in 1:length(cmpds)) {
-#      RT <- sapply(split(cmpd.samp.dfs[[cmpds[i]]]$Retention.Time,
-#                         cmpd.samp.dfs[[cmpds[i]]]$Replicate.Name),
-#                         mean, na.rm = T)
-#      RT.matrix <- cbind(RT.matrix, RT)
-#      colnames(RT.matrix)[ncol(RT.matrix)] <- as.character(cmpds[i])
-# }
-# 
-# ## Convert short format matrix to long form ----------------------
-# output <- melt(RT.matrix, value.name = "Retention Time")
-# colnames(output) <- c("Replicate.Name", "Compound.Name", "Retention.Time")
-# 
-# ## Add areas, heights, and ion ratios to output ----------------------
-# samp.data$S.N <- ((samp.data$Area + samp.data$Background) / samp.data$Background)
-# 
-# output <- full_join(output, samp.data[, c("Replicate.Name", "Precursor.Ion.Name",
-#                                          "Area", "Height", "Background",
-#                                          "Mass.Error.PPM", "S.N")], 
-#                     by = c("Replicate.Name", "Compound.Name" = "Precursor.Ion.Name"))
-# output <- output %>%
-#      rename(ppm = Mass.Error.PPM) %>%
-#      mutate(Notes = "",
-#             rawArea = Area,
-#             AreaBlkSub = Area,
-#             BlkRatio = NA)
-# 
-# ## Is it overloaded? ---------------------------------------
-# # Check peak height, if it is > max.height then keep the area data but make a note that it may be overloaded.
-# for (i in 1:nrow(output)) {
-#      if (!is.na(output$Height[i]) & output$Height[i] > max.height) {
-#           output$Notes[i] <- paste(output$Notes[i], "overloaded?", sep = "")
-#      }
-# }
-# 
-# ## Absolute height check  ------
-# for (i in 1:nrow(output)) {
-#      if (!is.na(output$Height[i]) & output$Height[i] < min.height) {
-#           output$Area[i] <- NA
-#           output$Notes[i] <- paste(output$Notes[i], "too small", sep = "")
-#      }
-# }
-# 
-# ## Signal to Noise check -----------------------------------------------
-# # If S/N is below threshold, throw it out!
-# for (i in 1:nrow(output)) {
-#   if (!is.na(output$S.N[i]) & output$S.N[i] < SN.thresh) {
-#     output$Area[i] <- NA
-#     output$Notes[i] <- paste(output$Notes[i], "bad S/N", sep = "")
-#     
-#   }
-# }
-# 
-# ## Parts per million check -----------------------------------------------
-# # If ppm is greater than threshold, throw it out!
-# for (i in 1:nrow(output)) {
-#   if (!is.na(output$ppm[i]) & abs(output$ppm[i]) > ppm.thresh) {
-#     output$Notes[i] <- paste(output$Notes[i], "bad ppm", sep = "")
-#     output$Area[i] <- NA
-#   }
-# }
-# 
-# ## Blank check -----------------------------------------------
-# # If area is not much greater than blank, throw it out!
-# for (i in 1:nrow(output)) {
-#      key <- as.character(output$Compound.Name[i])
-#      if (!is.na(output$Area[i]) & (output$Area[i] * blk.thresh) < mean(blk.range[, key])) {
-#           output$Notes[i] <- paste(output$Notes[i], "comparable to blank", sep = "")
-#           output$AreaBlkSub[i] <- output$Area[i] - mean(blk.range[, key])
-#           output$BlkRatio[i] <-  mean(blk.range[, key]) / output$Area[i]
-#           output$Area[i] <- NA
-#      }
-# }
-# 
-# ## RT range check --------------
-# for (i in 1:nrow(output)) { 
-#      key <- as.character(output$Compound.Name[i])
-#      if (!is.na(output$Retention.Time[i]) & output$Retention.Time[i] > max(RT.ok[ , key])) {
-#          output$Notes[i] <- paste(output$Notes[i], "Bad RT", sep = "")
-#          output$Area[i] <- NA
-#      }
-# }
-# 
-# for (i in 1:nrow(output)) { 
-#   key <- as.character(output$Compound.Name[i])
-#   if (!is.na(output$Retention.Time[i]) & 
-#       output$Retention.Time[i] < min(RT.ok[, key])) {
-#       output$Notes[i] <- paste(output$Notes[i], "Bad RT", sep = "")
-#       output$Area[i] <- NA
-#   }
-# }
-# 
-# ## Reinput all data from the internal standards-------------
-# output <- full_join(output, samp.data[, c("Replicate.Name", "Precursor.Ion.Name", "Protein.Name")],
-#                     by = c("Replicate.Name", "Compound.Name" = "Precursor.Ion.Name")) %>%
-#           rename(Compound.Type = Protein.Name) %>%
-#           mutate(Compound.Type = ifelse(grepl("Internal", Compound.Type), "Internal Stds", Compound.Type))
-# 
-# for (i in 1:nrow(output)) {
-#   if (!is.na(output$Compound.Type[i]) & output$Compound.Type[i] == "Internal Stds") {
-#     output$Notes[i] <- paste(output$Notes[i], "internal standard", sep = "")
-#     if (!grepl(pattern = "loaded", output$Notes[i])) {
-#       output$Area[i] <- output$rawArea[i]
-#     }
-#   }
-# }
-# 
-# ## Attach blank data to output ---------------------------
-# blank.data$Compound.Name <- blank.data$Precursor.Ion.Name
-# blank.data$Notes <- rep("Blank used for comparison", nrow(blank.data))
-# blank.data$S.N <- (blank.data$Area+blank.data$Background) / blank.data$Background
-# blank.data$rawArea <- blank.data$Area
-# blank.data$ppm <- blank.data$Mass.Error.PPM
-# blank.data$Compound.Type <- blank.data$Protein.Name
-# blank.data$AreaBlkSub <- blank.data$Area
-# blank.data$BlkRatio <- NA
-# final.output <- rbind(output, blank.data[, colnames(output)])
-# 
-#      
-# ## Output with comment-------------------------
-# # Ion name, area, was a peak removed?
-# comment.text <- paste("# Hello! welcome to your data! ", "Overload height: ", 
-#                       max.height, ". ", "RT flexibility: ", RT.flex, ". ",
-#                       "Blank can be this fraction of a sample: ",blk.thresh, ". ", 
-#                       "S/N threshold: " , SN.thresh, ". ",
-#                       "Minimum peak height: ", min.height, ". ",
-#                       "Processed on: ", Sys.time(), sep = "")
-# new.filename <- paste("QEQC_output", filename, sep = "")
-# con <- file(new.filename, open = "wt")
-# writeLines(paste(comment.text), con)
-# write.csv(final.output, con)
-# close(con)
-# 
+#
+# # ## RT range matrix ------------------------------
+# # RT.ok <- RT.range
+# # RT.ok[1, ] <- RT.range[1, ] - RT.flex
+# # RT.ok[2, ] <- RT.range[2, ] + RT.flex
+# #
+# # # Get the sample RTs
+# # RT.matrix <- c()
+# # for (i in 1:length(cmpds)) {
+# #      RT <- sapply(split(cmpd.samp.dfs[[cmpds[i]]]$Retention.Time,
+# #                         cmpd.samp.dfs[[cmpds[i]]]$Replicate.Name),
+# #                         mean, na.rm = T)
+# #      RT.matrix <- cbind(RT.matrix, RT)
+# #      colnames(RT.matrix)[ncol(RT.matrix)] <- as.character(cmpds[i])
+# # }
+# #
+# # ## Convert short format matrix to long form ----------------------
+# # output <- melt(RT.matrix, value.name = "Retention Time")
+# # colnames(output) <- c("Replicate.Name", "Compound.Name", "Retention.Time")
+# #
+# # ## Add areas, heights, and ion ratios to output ----------------------
+# # samp.data$S.N <- ((samp.data$Area + samp.data$Background) / samp.data$Background)
+# #
+# # output <- full_join(output, samp.data[, c("Replicate.Name", "Precursor.Ion.Name",
+# #                                          "Area", "Height", "Background",
+# #                                          "Mass.Error.PPM", "S.N")],
+# #                     by = c("Replicate.Name", "Compound.Name" = "Precursor.Ion.Name"))
+# # output <- output %>%
+# #      rename(ppm = Mass.Error.PPM) %>%
+# #      mutate(Notes = "",
+# #             rawArea = Area,
+# #             AreaBlkSub = Area,
+# #             BlkRatio = NA)
+# #
+# # ## Is it overloaded? ---------------------------------------
+# # # Check peak height, if it is > max.height then keep the area data but make a note that it may be overloaded.
+# # for (i in 1:nrow(output)) {
+# #      if (!is.na(output$Height[i]) & output$Height[i] > max.height) {
+# #           output$Notes[i] <- paste(output$Notes[i], "overloaded?", sep = "")
+# #      }
+# # }
+# #
+# # ## Absolute height check  ------
+# # for (i in 1:nrow(output)) {
+# #      if (!is.na(output$Height[i]) & output$Height[i] < min.height) {
+# #           output$Area[i] <- NA
+# #           output$Notes[i] <- paste(output$Notes[i], "too small", sep = "")
+# #      }
+# # }
+# #
+# # ## Signal to Noise check -----------------------------------------------
+# # # If S/N is below threshold, throw it out!
+# # for (i in 1:nrow(output)) {
+# #   if (!is.na(output$S.N[i]) & output$S.N[i] < SN.thresh) {
+# #     output$Area[i] <- NA
+# #     output$Notes[i] <- paste(output$Notes[i], "bad S/N", sep = "")
+# #
+# #   }
+# # }
+# #
+# # ## Parts per million check -----------------------------------------------
+# # # If ppm is greater than threshold, throw it out!
+# # for (i in 1:nrow(output)) {
+# #   if (!is.na(output$ppm[i]) & abs(output$ppm[i]) > ppm.thresh) {
+# #     output$Notes[i] <- paste(output$Notes[i], "bad ppm", sep = "")
+# #     output$Area[i] <- NA
+# #   }
+# # }
+# #
+# # ## Blank check -----------------------------------------------
+# # # If area is not much greater than blank, throw it out!
+# # for (i in 1:nrow(output)) {
+# #      key <- as.character(output$Compound.Name[i])
+# #      if (!is.na(output$Area[i]) & (output$Area[i] * blk.thresh) < mean(blk.range[, key])) {
+# #           output$Notes[i] <- paste(output$Notes[i], "comparable to blank", sep = "")
+# #           output$AreaBlkSub[i] <- output$Area[i] - mean(blk.range[, key])
+# #           output$BlkRatio[i] <-  mean(blk.range[, key]) / output$Area[i]
+# #           output$Area[i] <- NA
+# #      }
+# # }
+# #
+# # ## RT range check --------------
+# # for (i in 1:nrow(output)) {
+# #      key <- as.character(output$Compound.Name[i])
+# #      if (!is.na(output$Retention.Time[i]) & output$Retention.Time[i] > max(RT.ok[ , key])) {
+# #          output$Notes[i] <- paste(output$Notes[i], "Bad RT", sep = "")
+# #          output$Area[i] <- NA
+# #      }
+# # }
+# #
+# # for (i in 1:nrow(output)) {
+# #   key <- as.character(output$Compound.Name[i])
+# #   if (!is.na(output$Retention.Time[i]) &
+# #       output$Retention.Time[i] < min(RT.ok[, key])) {
+# #       output$Notes[i] <- paste(output$Notes[i], "Bad RT", sep = "")
+# #       output$Area[i] <- NA
+# #   }
+# # }
+# #
+# # ## Reinput all data from the internal standards-------------
+# # output <- full_join(output, samp.data[, c("Replicate.Name", "Precursor.Ion.Name", "Protein.Name")],
+# #                     by = c("Replicate.Name", "Compound.Name" = "Precursor.Ion.Name")) %>%
+# #           rename(Compound.Type = Protein.Name) %>%
+# #           mutate(Compound.Type = ifelse(grepl("Internal", Compound.Type), "Internal Stds", Compound.Type))
+# #
+# # for (i in 1:nrow(output)) {
+# #   if (!is.na(output$Compound.Type[i]) & output$Compound.Type[i] == "Internal Stds") {
+# #     output$Notes[i] <- paste(output$Notes[i], "internal standard", sep = "")
+# #     if (!grepl(pattern = "loaded", output$Notes[i])) {
+# #       output$Area[i] <- output$rawArea[i]
+# #     }
+# #   }
+# # }
+# #
+# # ## Attach blank data to output ---------------------------
+# # blank.data$Compound.Name <- blank.data$Precursor.Ion.Name
+# # blank.data$Notes <- rep("Blank used for comparison", nrow(blank.data))
+# # blank.data$S.N <- (blank.data$Area+blank.data$Background) / blank.data$Background
+# # blank.data$rawArea <- blank.data$Area
+# # blank.data$ppm <- blank.data$Mass.Error.PPM
+# # blank.data$Compound.Type <- blank.data$Protein.Name
+# # blank.data$AreaBlkSub <- blank.data$Area
+# # blank.data$BlkRatio <- NA
+# # final.output <- rbind(output, blank.data[, colnames(output)])
+# #
+# #
+# # ## Output with comment-------------------------
+# # # Ion name, area, was a peak removed?
+# # comment.text <- paste("# Hello! welcome to your data! ", "Overload height: ",
+# #                       max.height, ". ", "RT flexibility: ", RT.flex, ". ",
+# #                       "Blank can be this fraction of a sample: ",blk.thresh, ". ",
+# #                       "S/N threshold: " , SN.thresh, ". ",
+# #                       "Minimum peak height: ", min.height, ". ",
+# #                       "Processed on: ", Sys.time(), sep = "")
+# # new.filename <- paste("QEQC_output", filename, sep = "")
+# # con <- file(new.filename, open = "wt")
+# # writeLines(paste(comment.text), con)
+# # write.csv(final.output, con)
+# # close(con)
+# #
